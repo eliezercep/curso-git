c4231e707a679eaaec1f6b430451b22255c4b507
Eliezer Mottin <eliezer.apple.mottin@gmail.com>
2021-08-17 08:36:16 -0300

    Acrescentada uma mensagem de Bom dia

--- ProjetoTeste/Program.cs
+++ ProjetoTeste/Program.cs
@@ -7,7 +7,9 @@ namespace ProjetoTeste
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
+            Console.WriteLine("Bom dia!");
             Console.ReadLine();
+
         }
     }
 }
