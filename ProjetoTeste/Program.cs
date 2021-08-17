9c28fc5c625d64d1c004ea19f884c3f206ad941b
Eliezer Mottin <eliezer.apple.mottin@gmail.com>
2021-08-17 10:50:17 -0300

    acrescentada a mensagem desejando um bom dia

--- ProjetoTeste/Program.cs
+++ ProjetoTeste/Program.cs
@@ -6,9 +6,9 @@ namespace ProjetoTeste
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
-            Console.WriteLine("Bom dia!");
+            Console.WriteLine("Hello World!");            
             Console.WriteLine("Como você está hoje?");
+            Console.WriteLine("Tenha um Bom dia!");
             Console.ReadLine();
 
         }
