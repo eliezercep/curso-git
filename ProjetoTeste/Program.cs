9307a186a344a4cc7bd809a2301a6ed89387d580
Eliezer Mottin <eliezer.apple.mottin@gmail.com>
2021-08-18 17:05:25 -0300

    acrescetado mensagem de despedido

--- ProjetoTeste/Program.cs
+++ ProjetoTeste/Program.cs
@@ -9,6 +9,7 @@ namespace ProjetoTeste
             Console.WriteLine("Hello World!");            
             Console.WriteLine("Como você está hoje?");
             Console.WriteLine("Tenha um Bom dia!");
+            Console.WriteLine("Tchau!");
             Console.ReadLine();
 
         }
